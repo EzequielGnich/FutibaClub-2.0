94c7a13077b1fa5328cf0c5f9203827d59a25c0d
EzequielGnich <ezequielgnich@live.com>
2019-09-19 11:33:55 -0300

    Adicionado os primeiros end points da aplicação

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const routes = express.Router();
 
-const Account = require("../controllers/Users/account");
+const Account = require("../controllers/user/account");
 
+// Rotas que se referem a usuarios
 routes.get("/user", Account.index);
 routes.post("/user/newAccount", Account.store);
+
+// Rotas que se referem a login e logout
+routes.get("/login");
+routes.get("/logout");
+
+// Rotas que se referem aos jogos
+routes.get("/game");
+routes.post("/game");
+routes.delete("/game");
+routes.post("/game/results");
+
+// Rotas que busca a classificação
+routes.get("classification");
+
+// Rotas de controle de grupos
+routes.get("/:id/pending/:idGU/:op"); //Aceitar ou negar a entrada de uma pessoa no grupo no qual eu sou owner
+routes.delete("/group/:id");
+
+// Inserção de palpites
+routes.post("/guess");
 
 module.exports = routes;
