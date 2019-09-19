28f06be8a5f4eb63346e1b3bc21ed5207342df98
EzequielGnich <ezequielgnich@live.com>
2019-09-19 12:09:01 -0300

    Adicionado timestamps ao Schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,27 +1,32 @@
 const { Schema, model } = require("mongoose");
 
-const User = new Schema({
-  name: {
-    type: String,
-    required: true
+const User = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: Number,
+      required: true
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["OWNER", "USER"]
+    }
   },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  phoneNumber: {
-    type: Number,
-    required: true
-  },
-  role: {
-    type: String,
-    required: true,
-    enum: ["OWNER", "USER"]
+  {
+    timestamps: true
   }
-});
+);
 
 module.exports = model("Users", User);
