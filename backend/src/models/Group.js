2ec2a7a8d10ede9c2f3fe1c1492e9628977dd08a
EzequielGnich <ezequielgnich@live.com>
2019-09-23 12:59:58 -0300

    Criado model de Group

--- backend/src/models/Group.js
+++ backend/src/models/Group.js
@@ -1,7 +1,19 @@
 const { Schema, model } = require("mongoose");
 
 const Group = new Schema(
-  {},
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+      }
+    ]
+  },
   {
     timestamps: true
   }
