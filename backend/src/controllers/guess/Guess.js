ced93c37523885e485f04925d4b56fcc4f1b4ac3
EzequielGnich <ezequielgnich@live.com>
2019-09-27 12:33:23 -0300

    adicionado a função para adicionar o id do guess na collection games

--- backend/src/controllers/guess/Guess.js
+++ backend/src/controllers/guess/Guess.js
@@ -2,7 +2,7 @@ const Guess = require("./GuessModel");
 const Game = require("../game/GameModel");
 
 module.exports = {
-  async store(req, res) {
+  async store(req, res, next) {
     const { guess_teamA, guess_teamB, game_id, group_id } = req.body;
     const { user_id } = req.headers;
 
@@ -20,10 +20,41 @@ module.exports = {
         .json({ error: "Não foi possivel salvar o palpite" });
     }
 
+    // adiciona o guess ao jogo
+
+    const gameExists = await Game.findById(game_id);
+
+    if (!gameExists)
+      return res
+        .status(400)
+        .json({ error: "Jogo não existe na nossa base de dados" });
+
+    const guessExistsInGame = await gameExists.guessings.includes(guess._id);
+
+    if (!guessExistsInGame) {
+      gameExists.guessings.push(guess._id);
+
+      await gameExists.save();
+    } else {
+      return res.status(400).json({ error: "Palpite já cadastrado" });
+    }
+
+    next();
+
+    // fim adiciona
+
     res.status(200).send(guess);
   },
 
   async delete(req, res) {
     const { _id } = req.body;
+
+    if (!_id) {
+      return res.status(400).json({ error: "Palpite não encontrado" });
+    }
+
+    await Guess.findByIdAndDelete({ _id });
+
+    return res.status(200).json({ error: "Palpite apagado com sucesso" });
   }
 };
